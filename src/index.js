034bfc0005332dd0d84f18f664c9bedd60fd2ac7
Drake Costa <Saeris@comcast.net>
2018-08-19 01:59:59 -0700

    Add Auto-Install Support for Vue, Add unpkg Support, Update Demo Deps
    Auto-Install reference:
    https://vuejs.org/v2/cookbook/packaging-sfc-for-npm.html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import * as Components from './components'
 
+export function install(Vue) {
+  if (install.installed) return
+  install.installed = true
+  Object.entries(Components).forEach(([name, component]) => {
+    Vue.component(name, component)
+  })
+}
+
+const plugin = {
+  install
+}
+
+let GlobalVue = null
+if (typeof window !== `undefined`) {
+  GlobalVue = window.Vue
+} else if (typeof global !== `undefined`) {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
 export default Components
-export * from "./components"
+export * from './components'
